3687616b7940a4a3cdee24d35b7d4e35c01e2cde
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2017-01-31 13:16:41 +0530

    Updated script

--- DeployDbDemo/Scripts/deploy.ps1
+++ DeployDbDemo/Scripts/deploy.ps1
@@ -3,7 +3,7 @@
                 #--Load the Dac.dll into the PowerShell session--
 		Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll"
                 #--Load the SQL Server instance--
-		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=(local)"
+		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "Server=SISAHDLPT067;Database=Development;User ID=sa;Password=sigma@123;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"
                 #--Load the DACPAC--
 		$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\DeployDBDemo\bin\Release\DeployDBDemo.dacpac")
                 #--Deploy your database--
