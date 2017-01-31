a360dcc527135e21d1f3d966d8c8f5ce2b26c478
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2017-01-31 16:43:31 +0530

    Employee Table Updated

--- DeployDbDemo/dbo/Tables/Employees.sql
+++ DeployDbDemo/dbo/Tables/Employees.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Employees] (
-    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [EmployeeId]        INT           IDENTITY (1, 1) NOT NULL,
     [FirstName] NVARCHAR (50) NULL,
     [LastName]  NVARCHAR (50) NULL,
-    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([Id] ASC)
+    [Title] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
 );
 
