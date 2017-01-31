d2e922084e56634da3e96544df64a6725105a752
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2017-01-31 10:55:17 +0530

    Initial Changes

---
+++ DeployDbDemo/dbo/Tables/Employees.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50) NULL,
+    [LastName]  NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
