98fd1665ab01f8bfada1445fe3095ee8f062fb07
SIGMAINFOAHD\kutbuddin.s <kutbuddin.s@sigmainfo.net>
2017-02-01 11:40:53 +0530

    New Changes

--- DeployDbDemo/Scripts/deploy.ps1
+++ DeployDbDemo/Scripts/deploy.ps1
@@ -1,10 +1,14 @@
 ﻿#--TeamCity checkout directory command line argument--
-		param([string]$tcDir = "")
-                #--Load the Dac.dll into the PowerShell session--
-		Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll"
-                #--Load the SQL Server instance--
-		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "Server=SISAHDLPT067;Database=Development;User ID=sa;Password=sigma@123;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;"
-                #--Load the DACPAC--
-		$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\DeployDBDemo\bin\Release\DeployDBDemo.dacpac")
-                #--Deploy your database--
-		$dacService.deploy($dacPac, "QA", "True") 
+param([string]$tcDir = "")
+        
+#--Load the Dac.dll into the PowerShell session--
+Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll"
+
+#--Load the SQL Server instance--
+$dacService = new-object Microsoft.SqlServer.Dac.DacServices "Server=SISAHDLPT067;User Id=sa;Password=sigma@123;"
+
+#--Load the DACPAC--
+$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\DeployDBDemo\bin\Release\DeployDBDemo.dacpac")
+        
+#--Deploy your database--
+$dacService.deploy($dacPac, "QA", "True")
